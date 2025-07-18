089c14981f079da239d60f3c057a4b1e1fbae8cd
BigGunBrother <nokelove1111@gmail.com>
2025-07-19 07:15:59 +0800

    fix: 調整eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-    },
-  },
 ];
 
 export default eslintConfig;
