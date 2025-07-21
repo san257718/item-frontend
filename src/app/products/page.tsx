fc8be184a4f25370c0ede5c1f69048f87a57fca7
BigGunBrother <nokelove1111@gmail.com>
2025-07-21 21:53:03 +0800

    fix: 測試dashboard 畫面載入問題

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,17 +3,15 @@ import { getDashboardData } from "../use_server/getDashboardData";
 export const dynamic = "force-dynamic";
 export default async function Products() {
   const data = await getDashboardData();
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+
 
   return (
     <div>
       <div>
-        <div>{data[0].edited_today}</div>
-        <div>{data[0].active_users}</div>
-        <div>{data[0].pending_orders}</div>
-        <div>{data[0].total_number_of_products}</div>
+        <div>{data.length === 0 ? "-" : data[0].edited_today}</div>
+        <div>{data.length === 0 ? "-" : data[0].active_users}</div>
+        <div>{data.length === 0 ? "-" : data[0].pending_orders}</div>
+        <div>{data.length === 0 ? "-" : data[0].total_number_of_products}</div>
       </div>
     </div>
   );
